ca998630a43a5f697e3afe2fd8a0b66b4a3c8db5
AlessandraParziale <96733892+AlessandraParziale@users.noreply.github.com>
2021-12-28 10:30:33 +0100

    Utente,ListaPreferiti

--- src/main/java/Model/ListaPreferiti/ListaPreferitiBean.java
+++ src/main/java/Model/ListaPreferiti/ListaPreferitiBean.java
@@ -1,4 +1,41 @@
 package Model.ListaPreferiti;
 
+import Model.Corso.CorsoBean;
+
+import java.util.ArrayList;
+
 public class ListaPreferitiBean {
+
+    private int idLista;
+    private ArrayList<CorsoBean> corsi;
+
+    public ListaPreferitiBean() { }
+
+    public ListaPreferitiBean(int idLista, ArrayList<CorsoBean> corsi) {
+        this.idLista = idLista;
+        this.corsi = corsi;
+    }
+
+    public ListaPreferitiBean(ArrayList<CorsoBean> corsi) {
+        this.corsi = corsi;
+    }
+
+    public int getIdLista() {
+        return idLista;
+    }
+
+    public void setIdLista(int idLista) {
+        this.idLista = idLista;
+    }
+
+    public ArrayList<CorsoBean> getCorsi() {
+        return corsi;
+    }
+
+    public void setCorsi(ArrayList<CorsoBean> corsi) {
+        this.corsi = corsi;
+    }
+
+
+
 }
--- src/main/java/Model/Utente/UtenteBean.java
+++ src/main/java/Model/Utente/UtenteBean.java
@@ -1,4 +1,90 @@
 package Model.Utente;
 
+import java.time.LocalDate;
+
 public class UtenteBean {
+    private int idUtente;
+    private String nome;
+    private String cognome;
+    private String email;
+    private LocalDate ddn;
+    private String password;
+    private boolean tipo;
+
+    public UtenteBean(){ }
+
+    public UtenteBean(int idUtente, String nome, String cognome, String email, LocalDate ddn, String password, boolean tipo) {
+        this.idUtente = idUtente;
+        this.nome = nome;
+        this.cognome = cognome;
+        this.email = email;
+        this.ddn = ddn;
+        this.password = password;
+        this.tipo = tipo;
+    }
+
+    public UtenteBean(String nome, String cognome, String email, LocalDate ddn, String password, boolean tipo) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.email = email;
+        this.ddn = ddn;
+        this.password = password;
+        this.tipo = tipo;
+    }
+
+    public void setIdUtente(int idUtente) {
+        this.idUtente = idUtente;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setDdn(LocalDate ddn) {
+        this.ddn = ddn;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setTipo(boolean tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getIdUtente() {
+        return idUtente;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public LocalDate getDdn() {
+        return ddn;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public boolean isTipo() {
+        return tipo;
+    }
 }
