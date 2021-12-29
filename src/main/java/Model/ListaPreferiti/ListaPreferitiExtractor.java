c343dd00ad689f6d37e2799f7102053c200a799e
AlessandraParziale <96733892+AlessandraParziale@users.noreply.github.com>
2021-12-29 11:18:14 +0100

    Dao Utente, Lista

--- src/main/java/Model/ListaPreferiti/ListaPreferitiExtractor.java
+++ src/main/java/Model/ListaPreferiti/ListaPreferitiExtractor.java
@@ -1,4 +1,14 @@
 package Model.ListaPreferiti;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class ListaPreferitiExtractor {
+
+    public ListaPreferitiBean extract(ResultSet resultSet) throws SQLException {
+        ListaPreferitiBean l = new ListaPreferitiBean();
+        l.setIdUtente(resultSet.getInt("l.idUtente"));
+
+        return l;
+    }
 }
